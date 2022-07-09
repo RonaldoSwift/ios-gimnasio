4a28c6044302342ab5da9a3c0499846cf82db4ac
Ronaldo Andre <ronaldovh99@gmail.com>
2022-07-08 19:44:02 -0500

    Initial Commit

---
+++ ios-gimnasio/ios_gimnasioApp.swift
@@ -0,0 +1,20 @@
+//
+//  ios_gimnasioApp.swift
+//  ios-gimnasio
+//
+//  Created by Ronaldo Andre Vargas Huaman on 8/07/22.
+//
+
+import SwiftUI
+
+@main
+struct ios_gimnasioApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
