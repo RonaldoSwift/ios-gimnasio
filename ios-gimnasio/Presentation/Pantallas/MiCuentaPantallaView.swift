27ed6bf74609c52151be48500dd11dc4ff208cd7
Ronaldo Andre <ronaldovh99@gmail.com>
2022-07-20 13:03:20 -0500

    Trabajando en pantalla 3

--- ios-gimnasio/Presentation/Pantallas/MiCuentaPantallaView.swift
+++ ios-gimnasio/Presentation/Pantallas/MiCuentaPantallaView.swift
@@ -10,69 +10,21 @@ import SwiftUI
 struct MiCuentaPantallaView: View {
     var body: some View {
         
-        VStack(alignment: .center, spacing: 20){
-            Text("Mi Cuenta")
-                .padding()
-                .border(Color.black)
-            
-            HStack(alignment: .center, spacing: 30){
-                Image(systemName: "person.crop.circle.fill")
+        ZStack{
+            VStack{
+                Image("ImageSingUp")
                     .resizable()
-                    .foregroundColor(Color.green)
-                    .aspectRatio(contentMode: .fill)
-                    .frame(width: 70.0, height: 50.0, alignment: .center)
-                VStack{
-                    Text("Sofia Torres Leon")
-                        .bold()
-                        .font(.title2)
-                    Text("sofiaLeon@gmail.com")
-                        .foregroundColor(Color.black)
-                }
+                    .scaledToFit()
+                Spacer()
             }
-            Text("________________________________")
-                .foregroundColor(Color.blue)
+            ZStack{
+                Image("")
+                
+            }
             
-            VStack(alignment: .leading){
-                HStack{
-                    Image(systemName: "star.fill")
-                        .foregroundColor(Color.yellow)
-                    Text("Clases pendientes")
-                        .bold()
-                        .font(.title2)
-                }
-                
-                HStack{
-                    Image(systemName: "heart.fill")
-                        .foregroundColor(Color.red)
-                    Text("Evaluar esta aplicacion")
-                        .bold()
-                        .font(.title2)
-                }
-                
-                HStack{
-                    Image(systemName: "message.fill")
-                        .foregroundColor(Color.blue)
-                    Text("Correo")
-                        .bold()
-                        .font(.title2)
-                }
-                
-                HStack{
-                    Image(systemName: "network")
-                        .foregroundColor(Color.blue)
-                    Text("Redes Sociales")
-                        .bold()
-                        .font(.title2)
-                }
-                
-                Text("Politica de privasidad")
-                    .padding()
-                    .background(Color.yellow)
-                    .cornerRadius(6)
-            }
-            Spacer()
+            
         }
-        .padding()
+        .edgesIgnoringSafeArea(.all)
     }
 }
 
