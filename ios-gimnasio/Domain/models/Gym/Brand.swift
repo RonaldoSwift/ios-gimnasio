7aeab30a7740b7affb6cb447ddee0781615535f1
Carlos Vargas Huaman <cvargas@godaddy.com>
2022-07-28 14:25:22 -0500

    Pantalla de marcas

---
+++ ios-gimnasio/Domain/models/Gym/Brand.swift
@@ -0,0 +1,14 @@
+//
+//  Brand.swift
+//  ios-gimnasio
+//
+//  Created by Carlos Leonardo Camilo Vargas Huaman on 28/07/22.
+//
+
+import Foundation
+
+struct Brand: Identifiable {
+    let id: Int
+    let title: String
+    let image: ImageAsset.Image
+}
--- ios-gimnasio/Presentation/UI/Home/Components/GymPantallaView.swift
+++ ios-gimnasio/Presentation/UI/Home/Components/GymPantallaView.swift
@@ -8,45 +8,59 @@
 import SwiftUI
 
 struct GymPantallaView: View {
+    
+    let brands = [
+        Brand(id: 0, title: "Gold's Gym", image: Assets.Registro.googleIcono.image),
+        Brand(id: 1, title: "SmartFit", image: Assets.Registro.googleIcono.image),
+        Brand(id: 2, title: "Bodytech", image: Assets.Registro.googleIcono.image),
+        Brand(id: 3, title: "Megaforce", image: Assets.Registro.googleIcono.image),
+        Brand(id: 4, title: "Megatlon", image: Assets.Registro.googleIcono.image),
+        Brand(id: 5, title: "Gimnasio B2", image: Assets.Registro.googleIcono.image),
+        Brand(id: 6, title: "Aldo's Gym", image: Assets.Registro.googleIcono.image),
+        Brand(id: 7, title: "X Sport Gym", image: Assets.Registro.googleIcono.image),
+        Brand(id: 8, title: "Master Gym", image: Assets.Registro.googleIcono.image),
+        Brand(id: 9, title: "Fitness de Impacto", image: Assets.Registro.googleIcono.image),
+        Brand(id: 10, title: "Sportlife Fitness Club", image: Assets.Registro.googleIcono.image)
+    ]
+    
+    let layout = [
+        GridItem(.fixed(200)),
+        GridItem(.fixed(200))
+    ]
+    
     var body: some View {
-        ZStack{
-            Color("ColorDegradente")
-            VStack(alignment: .leading){
-                HStack(alignment: .center, spacing: 115){
-                    Text("<-")
-                        .foregroundColor(Color.white)
-                    Text("Sessions")
-                        .foregroundColor(Color.white)
-                    Image("imageDeChica")
-                }
-                
-                Text("Live Sessions")
-                    .foregroundColor(Color.white)
-                
-                ScrollView(.horizontal){
-                    HStack{
-                        Image("ImageMujerConLiga")
-                            .resizable()
-                            .frame(width: 319, height: 226, alignment: .center)
-                        Image("CuartoDeImagen")
-                            .resizable()
-                            .frame(width: 319, height: 226, alignment: .center)
+        ScrollView {
+            LazyVGrid(
+                columns: layout
+            ) {
+                ForEach(brands, id: \.id) { brand in
+                    NavigationLink(destination: GymSedesPantallaView()) {
+                        GymCard(brand: brand)
                     }
                 }
-                
-                HStack(alignment: .center, spacing: 240){
-                    Text("Trainers")
-                        .foregroundColor(Color.white)
-                    Text("See all")
-                        .foregroundColor(Color.white)
-                }
-                
-                
             }
-            .padding()
         }
-        .edgesIgnoringSafeArea(.all)
     }
+    
+    private func GymCard(brand: Brand) -> some View {
+        ZStack {
+            Image(uiImage: brand.image)
+                .resizable()
+                .padding()
+                .background(Color.black)
+                .cornerRadius(25)
+            
+            VStack {
+                Spacer()
+                Text(brand.title)
+                    .padding(5)
+                    .background(Color.green)
+            }
+        }
+        .padding(5)
+        .frame(height: 200)
+    }
+    
 }
 
 struct GymPantallaView_Previews: PreviewProvider {
